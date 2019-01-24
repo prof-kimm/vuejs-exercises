284d9fc9cd8175f242f2689c81327117e6c6a23e
prof-kimm <prof-kimm@algonquincollege.com>
2019-01-24 10:16:52 -0500

    save changes

--- src/components/PlayGround/PlayGround.js
+++ src/components/PlayGround/PlayGround.js
@@ -2,13 +2,41 @@ export default {
   name: "PlayGround",
   data() {
     return {
-      input: '',
+      input: "",
+      message: "some random message",
+      noCacheMessage: "some random message",
+      question: '',
+      answer: 'I cannot give you an answer until you ask a question!',
+    };
+  },
+  computed: {
+    reverseMessage() {
+      return this.noCacheMessage
+        .split("")
+        .reverse()
+        .join("");
     }
+  },
+  watch: {
+    question() {
+      this.answer = "Waiting for you to stop typing...";
+      setTimeout(() => {
+        this.answer = "time ran out!"; 
+      }, 4000);
+      // this.debouncedGetAnswer();
+    },
   },
   methods: {
     handleChange(value) {
       /* eslint-disable */
-      console.log('value', value);
+      console.log("value", value);
+    },
+    reverseMessageNotCached() {
+      // this.noCacheMessage = 'changedCacheMessage'
+      return this.message
+        .split("")
+        .reverse()
+        .join("");
     }
   }
 };
--- src/components/PlayGround/PlayGround.vue
+++ src/components/PlayGround/PlayGround.vue
@@ -1,10 +1,29 @@
 <template>
   <div class="hello">
-    <el-input 
-      placeholder="Please input"
-      v-model="input"
-      @change="handleChange">
-    </el-input>
+    <!-- v-bind -->
+    <!-- v-bind shorthand-->
+    <!-- <div>Some Class With Background</div> -->
+    <!-- v-on -->
+    <!-- v-on shorthand -->
+    <!-- <button>On Click</button> -->
+    <!-- v-once -->
+    <!-- <span v-once>This will never change: {{ msg }}</span> -->
+    <!-- <div>Some Text Change</div> -->
+    <!-- v-html -->
+    <!-- <p>Using mustaches: {{ rawHtml }}</p> -->
+    <!-- <p>Using v-html directive: <span v-html="rawHtml"></span></p> -->
+    <!-- v-model -->
+    <!-- <input type="text"> -->
+    <!-- computed -->
+    <div>{{ reverseMessage }}</div>
+    <div>{{ reverseMessageNotCached() }}</div>
+    <!-- computed property will immediately return the previously computed result without having to run the function again. -->
+    <!-- watch -->
+    <p>Ask a yes/no question:
+      <input v-model="question">
+    </p>
+    <p>{{ answer }}</p>
+
   </div>
 </template>
 <script src="./PlayGround.js"></script>
